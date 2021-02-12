69325c2d6c7dc063af8d672eafe5afc3698d4671
Harish Narain <harishnarain@gmail.com>
2021-02-13 08:36:53 +1100

    Added auth guard to CreateSubscription container.

--- src/containers/CreateSubscription/CreateSubscription.js
+++ src/containers/CreateSubscription/CreateSubscription.js
@@ -8,10 +8,13 @@ import clsx from 'clsx';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
-import { useMsal } from '@azure/msal-react';
+import { useMsal, MsalAuthenticationTemplate } from '@azure/msal-react';
+import { InteractionType } from '@azure/msal-browser';
 
 import { createSubscription } from '../../axios/index';
 import authScopes from '../../shared/auth/authScopes';
+import Spinner from '../../components/UI/Spinner';
+import Aux from '../../hoc/Aux/Aux';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -35,6 +38,28 @@ const CreateSubscription = () => {
   const { productId } = useParams();
   const [tenantName, setTenantName] = useState('');
   const loginScopes = { ...authScopes };
+
+  if (accounts.length > 0) {
+    const userName = accounts[0].username;
+    const currentAccount = instance.getAccountByUsername(userName);
+    const silentRequest = {
+      ...loginScopes,
+      account: currentAccount,
+      forceRefresh: false,
+    };
+
+    const request = {
+      ...loginScopes,
+      loginHint: currentAccount.username,
+    };
+
+    instance
+      .acquireTokenSilent(silentRequest)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch(() => useMsal.acquireTokenRedirect(request));
+  }
 
   const tenantNameInputHandler = (value) => {
     setTenantName(value);
@@ -72,31 +97,40 @@ const CreateSubscription = () => {
   };
 
   return (
-    <form className={classes.root} autoComplete="off" onSubmit={(event) => onSubmitHandler(event)}>
-      <Container>
-        <Grid
-          container
-          spacing={0}
-          direction="column"
-          alignItems="center"
-          justify="center"
-          className={classes.formGrid}>
-          <Grid item xs={3}>
-            <FormControl className={clsx(classes.margin, classes.textField)}>
-              <Input
-                endAdornment={<InputAdornment position="end">.onifiduk.com</InputAdornment>}
-                onChange={(event) => tenantNameInputHandler(event.target.value)}
-              />
-            </FormControl>
+    <Aux>
+      {/* prettier-ignore */}
+      <MsalAuthenticationTemplate
+        interactionType={InteractionType.Redirect}
+        loadingComponent={Spinner}
+        authenticationRequest={loginScopes}
+      >
+      <form className={classes.root} autoComplete="off" onSubmit={(event) => onSubmitHandler(event)}>
+        <Container>
+          <Grid
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justify="center"
+            className={classes.formGrid}>
+            <Grid item xs={3}>
+              <FormControl className={clsx(classes.margin, classes.textField)}>
+                <Input
+                  endAdornment={<InputAdornment position="end">.onifiduk.com</InputAdornment>}
+                  onChange={(event) => tenantNameInputHandler(event.target.value)}
+                />
+              </FormControl>
+            </Grid>
+            <Grid item xs={3}>
+              <Button variant="contained" color="primary" type="submit">
+                Deploy
+              </Button>
+            </Grid>
           </Grid>
-          <Grid item xs={3}>
-            <Button variant="contained" color="primary" type="submit">
-              Deploy
-            </Button>
-          </Grid>
-        </Grid>
-      </Container>
-    </form>
+        </Container>
+      </form>
+      </MsalAuthenticationTemplate>
+    </Aux>
   );
 };
 
